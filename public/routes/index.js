53292e9bd0e314282a150a6ebacb876528692a46
amit <amitmakwana7563@gmail.com>
2026-06-20 16:02:32 +0530

    Remove unnecessary file.

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,12 +1,10 @@
 import userRouter from "./user.js"
 import adminRouter from "./admin.js"
-import taskRouter from "./task.js"
 import { Router } from "express";
 
 const router = new Router();
 
 router.use("/user", userRouter)
 router.use("/admin", adminRouter)
-router.use("/task", taskRouter);
 
 export default router;
