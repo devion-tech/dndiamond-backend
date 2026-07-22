74bf450ed6566dc6fb0a989310d2b16a8e374daf
Maulik1742 <mauliksolanki2002@gmail.com>
2026-07-22 18:11:27 +0530

    Changes in attribute API

--- public/routes/attribute.js
+++ public/routes/attribute.js
@@ -1,18 +1,39 @@
 import { Router } from "express";
 import * as attributeController from "../controllers/attributes.js";
 import { verifyAdminToken } from "../utills/jwt.helper.js";
-import { validateRequest, validateRequestForParams } from "../middelware/validation.js";
-import { createAttributeValidation, updateAttributeValidation } from "../validation/attributes.js";
+import {
+  validateRequest,
+  validateRequestForParams,
+} from "../middelware/validation.js";
+import {
+  createAttributeValidation,
+  updateAttributeValidation,
+} from "../validation/attributes.js";
 import { commonIdValidation } from "../validation/common.js";
 
 const router = new Router();
 
-router.get("/", verifyAdminToken, attributeController.getAttributes);
+router.get("/", attributeController.getAttributes);
 
-router.post("/", verifyAdminToken, validateRequest(createAttributeValidation), attributeController.createAttribute);
+router.post(
+  "/",
+  verifyAdminToken,
+  validateRequest(createAttributeValidation),
+  attributeController.createAttribute,
+);
 
-router.put("/:id", verifyAdminToken, validateRequest(updateAttributeValidation), attributeController.updateAttribute);
+router.put(
+  "/:id",
+  verifyAdminToken,
+  validateRequest(updateAttributeValidation),
+  attributeController.updateAttribute,
+);
 
-router.delete("/:id", verifyAdminToken, validateRequestForParams(commonIdValidation), attributeController.deleteAttribute);
+router.delete(
+  "/:id",
+  verifyAdminToken,
+  validateRequestForParams(commonIdValidation),
+  attributeController.deleteAttribute,
+);
 
 export default router;
