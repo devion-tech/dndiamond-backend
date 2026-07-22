2b2f898a97797e1b4fedd8e96ccd7ba4570cb714
amit <amitmakwana7563@gmail.com>
2026-07-22 15:34:33 +0530

    Live url add in live server main branch.

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 dotenv.config()
 let live_url = process.env.LIVE_URL
 let local_url = process.env.LOCAL_URL
-mongoose.connect(local_url).then(() => {
+mongoose.connect(live_url).then(() => {
     console.log('Connection succesfull!');
 }).catch((error) => {
     console.log("not connect", error);
