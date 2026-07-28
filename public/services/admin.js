fd5b11f3e4b656cc9f563f58aefae433bcff5a45
amit <amitmakwana7563@gmail.com>
2026-07-28 10:19:50 +0530

    .

--- public/services/admin.js
+++ public/services/admin.js
@@ -80,7 +80,7 @@ export const getDashboard = async () => {
       .limit(5),
     DiamondInquiry.find({ is_deleted: 0, })
       .populate("user_id", "name email")
-      .select("_id createdAt user_id budget_min budget_max")
+      .select("_id createdAt user_id budget_min budget_max additional_notes")
       .sort({ createdAt: -1, })
       .limit(5),
   ]);
